c6d0530d435304e418fd924c5b5438982a32e89f
sthAneal <anilmeronaam@gmail.com>
2023-06-18 12:55:30 +1000

    video removed

--- wp-content/themes/WNW/index.php
+++ wp-content/themes/WNW/index.php
@@ -3,12 +3,13 @@ get_header();
 ?>
 <main>
   <!-- The video -->
-  <div class="video-wrapper">
+  <!-- <div class="video-wrapper">
     <video autoplay muted loop id="featureVideo">
       <source src="<?php path_to('media/hair-cut.mp4') ?>" type="video/mp4">
     </video>
     <div></div>
-  </div>
+  </div> -->
+  
 </main>
 
 <?php
