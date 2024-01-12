cfa5fbf8385435ac15e31e979c7704bb58c3a6ba
SthAneal <anilmeronaam@gmail.com>
2024-01-12 16:55:52 +1100

    single page content managed

--- wp-content/themes/WNW/single.php
+++ wp-content/themes/WNW/single.php
@@ -1,15 +1,28 @@
 <?php
 get_header();
 ?>
-<div>
-   <?php
-   while (have_posts()):
-      the_post();
-      echo the_title().'<br/>';
-      echo the_content();
-   endwhile;
-   ?>
-</div>
+<main id="aboutus">
+  <?php
+  if (have_posts()):
+    the_post();
+    ?>
+    <div class="info">
+      <div class="container">
+        <h2>
+          <?php echo the_title(); ?>
+        </h2>
+        <div>
+          <?php
+          echo the_content();
+          ?>
+        </div>
+      </div>
+    </div>
+    <?php
+  endif;
+  ?>
+
+</main>
 
 <?php
 get_footer();
